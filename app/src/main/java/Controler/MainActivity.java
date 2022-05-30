c64db7379a7ba77d90e6885c8519ec8f4023690b
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-05-30 14:59:59 +0200

    create link to play activity

--- app/src/main/java/Controler/MainActivity.java
+++ app/src/main/java/Controler/MainActivity.java
@@ -63,4 +63,11 @@ public class MainActivity  extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Permet une animation de la vue (override le comportement de base)
     }
 
+    public void play(View view){
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Permet une animation de la vue (override le comportement de base)
+
+    }
+
 }
