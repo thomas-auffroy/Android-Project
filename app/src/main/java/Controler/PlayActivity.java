2cece079f1cc91e94c512e2979d8159ab97c4c76
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-06-01 16:10:10 +0200

    add onstart and backward methods

--- app/src/main/java/Controler/PlayActivity.java
+++ app/src/main/java/Controler/PlayActivity.java
@@ -1,9 +1,12 @@
 package Controler;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -52,5 +55,21 @@ public class PlayActivity extends AppCompatActivity {
         getCategory.execute();
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        // Permet de cacher la barre de navigation du bas
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                //|View.SYSTEM_UI_FLAG_FULLSCREEN // Si on veut cacher la barre android du haut
+        );
+        super.onStart();
+    }
+
+    public void backward(View view){
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right); // Permet une animation de la vue (override le comportement de base)
     }
 }
