cfc899c9077587dfb1e25f397049d91d54884b64
Thomas Auffroy <thomas.auffroy@etu.univ-grenoble-alpes.fr>
2022-06-08 17:24:09 +0200

    add classes

--- app/src/main/java/Model/db/AppDatabase.java
+++ app/src/main/java/Model/db/AppDatabase.java
@@ -3,13 +3,15 @@ package Model.db;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class, Game.class, Score.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Game.class, Score.class, Question.class, Reponse.class}, version = 1, exportSchema = false)
 
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
     public abstract GameDao gameDao();
     public abstract ScoreDao scoreDao();
+    public abstract QuestionDao questionDao();
+    public abstract ReponseDao reponseDao();
 
 }
 
