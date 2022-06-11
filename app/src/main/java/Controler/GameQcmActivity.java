8d4bde6a63cd5cdfbc82538b3be4afde9596af46
Thomas <thomas.auffroy@gmail.com>
2022-06-11 20:49:07 +0200

    adapt activity to work with set of questions

--- app/src/main/java/Controler/GameQcmActivity.java
+++ app/src/main/java/Controler/GameQcmActivity.java
@@ -28,9 +28,13 @@ public class GameQcmActivity extends AppCompatActivity {
 
     private User user;
     private Game game;
+    private static List<Question> availableQuestions = null;
 
     private DatabaseClient mDb;
 
+    private TextView nameCategory;
+
+    private TextView numeroQuestionTextView;
     private TextView questionView;
     private Button answer1View;
     private Button answer2View;
@@ -40,113 +44,51 @@ public class GameQcmActivity extends AppCompatActivity {
 
     private boolean haveAnswered = false;
 
+    private static Integer numeroQuestion = 1;
+    private final Integer maxQuestions = 3;
+    private static Integer goodAnswers = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.template_game_qcm);
 
+        mDb = DatabaseClient.getInstance(getApplicationContext());
+        user = (User) getIntent().getSerializableExtra("USER");
+        game = (Game) getIntent().getSerializableExtra("GAME");
+
         questionView = findViewById(R.id.questionQCM);
+
+        if (availableQuestions == null) {
+            getAllQuestionsFromGameId(game.getId());
+        } else {
+            Question aQuestion = chooseRandomly();
+            questionView.setText(aQuestion.getQuestion());
+            displayAnswer(aQuestion);
+        }
+
+        nameCategory = findViewById(R.id.nameCategoryGameQcm);
+        nameCategory.setText(game.getCategorie());
+
+        numeroQuestionTextView = findViewById(R.id.numeroQuestion);
+        numeroQuestionTextView.setText("Question N°" + numeroQuestion + " sur " + maxQuestions);
+
         answer1View = findViewById(R.id.answerQCM1);
         answer2View = findViewById(R.id.answerQCM2);
         answer3View = findViewById(R.id.answerQCM3);
         answer4View = findViewById(R.id.answerQCM4);
 
         rightAnswer = "";
-
-        mDb = DatabaseClient.getInstance(getApplicationContext());
-
-        user = (User) getIntent().getSerializableExtra("USER");
-        game = (Game) getIntent().getSerializableExtra("GAME");
-
-        class GetNombreQuestions extends AsyncTask<Void, Void, Integer> {
-
-            @Override
-            protected Integer doInBackground(Void... voids) {
-                Integer nombre = mDb.getAppDatabase().questionDao().getNombreQuestions(game.getId());
-                return nombre;
-            }
-
-            @Override
-            protected void onPostExecute(Integer nombreQuestions) {
-                class GetQuestion extends AsyncTask<Void, Void, Question> {
-
-                    @Override
-                    protected Question doInBackground(Void... voids) {
-                        int random_int = (int) Math.floor(Math.random()*(nombreQuestions));
-
-                        Question question = mDb.getAppDatabase().questionDao().getQuestionFromIds(game.getId(), random_int);
-                        return question;
-                    }
-
-                    @Override
-                    protected void onPostExecute(Question question) {
-                        questionView.setText(question.getQuestion());
-
-                        class GetReponses extends AsyncTask<Void, Void, List<Reponse>> {
-
-                            @Override
-                            protected List<Reponse> doInBackground(Void... voids) {
-                                List<Reponse> reponses = mDb.getAppDatabase().reponseDao().getAllReponsesFromQuestionId(question.getQuestionId());
-                                return reponses;
-                            }
-
-                            @Override
-                            protected void onPostExecute(List<Reponse> reponses) {
-
-                                ListIterator<Reponse> it = reponses.listIterator();
-
-                                while (it.hasNext() && rightAnswer == ""){
-                                    Reponse buffer = it.next();
-                                    if (buffer.getEstVrai() == 1) {
-                                        rightAnswer = buffer.getReponse();
-                                    }
-                                }
-
-                                // Position aléatoirement les réponses
-                                ArrayList<Integer> buffer = new ArrayList<>();
-                                buffer.add(0);
-                                buffer.add(1);
-                                buffer.add(2);
-                                buffer.add(3);
-
-                                int random = (int) Math.floor(Math.random()*(4));
-                                answer1View.setText(reponses.get(buffer.get(random)).getReponse());
-                                buffer.remove(random);
-
-                                random = (int) Math.floor(Math.random()*(3));
-                                answer2View.setText(reponses.get(buffer.get(random)).getReponse());
-                                buffer.remove(random);
-
-                                random = (int) Math.floor(Math.random()*(2));
-                                answer3View.setText(reponses.get(buffer.get(random)).getReponse());
-                                buffer.remove(random);
-
-                                random = (int) Math.floor(Math.random()*(1));
-                                answer4View.setText(reponses.get(buffer.get(random)).getReponse());
-                                buffer.remove(random);
-                            }
-                        }
-
-                        GetReponses getReponses = new GetReponses();
-                        getReponses.execute();
-                    }
-                }
-
-                GetQuestion getQuestion = new GetQuestion();
-                getQuestion.execute();
-            }
-        }
-        GetNombreQuestions getNombreQuestions = new GetNombreQuestions();
-        getNombreQuestions.execute();
     }
 
 
-    public void checkAnswer(View view){
+    public void checkAnswer(View view) {
         Button btnClicked = findViewById(view.getId());
 
-        if (haveAnswered == false){
-            if (btnClicked.getText() == rightAnswer){
+        if (haveAnswered == false) {
+            if (btnClicked.getText() == rightAnswer) {
                 btnClicked.setTextColor(Color.GREEN);
+                goodAnswers += 1;
             } else {
                 btnClicked.setTextColor(Color.RED);
             }
@@ -154,11 +96,25 @@ public class GameQcmActivity extends AppCompatActivity {
         haveAnswered = true;
     }
 
-    public void playAgain(View view){
+    public void next(View view) {
 
         // Reload activty
-        finish();
-        startActivity(getIntent());
+        if (availableQuestions.size() > 0) {
+            numeroQuestion += 1;
+            finish();
+            startActivity(getIntent());
+        } else {
+            availableQuestions = null;
+            numeroQuestion = 1;
+
+            Intent intent = new Intent(this, ResultQcmActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("USER", user);
+            intent.putExtra("GAME", game);
+            intent.putExtra("FOO", goodAnswers);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Permet une animation de la vue (override le comportement de base)
+        }
     }
 
     @Override
@@ -166,14 +122,107 @@ public class GameQcmActivity extends AppCompatActivity {
         // Permet de cacher la barre de navigation du bas
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 //|View.SYSTEM_UI_FLAG_FULLSCREEN // Si on veut cacher la barre android du haut
         );
         super.onStart();
     }
 
-    public void backward(View view){
+    public void backward(View view) {
+        numeroQuestion = 1;
+        availableQuestions = null;
+
         super.finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right); // Permet une animation de la vue (override le comportement de base)
+    }
+
+
+    private void getAllQuestionsFromGameId(Integer gameId) {
+
+        class GetQuestions extends AsyncTask<Void, Void, List<Question>> {
+            @Override
+            protected List<Question> doInBackground(Void... voids) {
+                List<Question> questions = mDb.getAppDatabase().questionDao().getAllQuestionsFromGameId(gameId, maxQuestions);
+                return questions;
+            }
+
+            @Override
+            protected void onPostExecute(List<Question> questions) {
+                /*
+                ListIterator<Question> it = questions.listIterator();
+                while (it.hasNext()) {
+                    System.out.println(it.next());
+                }
+                */
+
+                availableQuestions = questions;
+                Question aQuestion = chooseRandomly();
+                questionView.setText(aQuestion.getQuestion());
+                displayAnswer(aQuestion);
+            }
+        }
+        GetQuestions getQuestion = new GetQuestions();
+        getQuestion.execute();
+
+    }
+
+    private void displayAnswer(Question question) {
+
+        class GetReponses extends AsyncTask<Void, Void, List<Reponse>> {
+
+            @Override
+            protected List<Reponse> doInBackground(Void... voids) {
+                List<Reponse> reponses = mDb.getAppDatabase().reponseDao().getAllReponsesFromQuestionId(question.getQuestionId());
+                return reponses;
+            }
+
+            @Override
+            protected void onPostExecute(List<Reponse> reponses) {
+
+                ListIterator<Reponse> it = reponses.listIterator();
+
+                while (it.hasNext() && rightAnswer == "") {
+                    Reponse buffer = it.next();
+                    if (buffer.getEstVrai() == 1) {
+                        rightAnswer = buffer.getReponse();
+                    }
+                }
+
+                // Position aléatoirement les réponses
+                ArrayList<Integer> buffer = new ArrayList<>();
+                buffer.add(0);
+                buffer.add(1);
+                buffer.add(2);
+                buffer.add(3);
+
+                int random = (int) Math.floor(Math.random() * (4));
+                answer1View.setText(reponses.get(buffer.get(random)).getReponse());
+                buffer.remove(random);
+
+                random = (int) Math.floor(Math.random() * (3));
+                answer2View.setText(reponses.get(buffer.get(random)).getReponse());
+                buffer.remove(random);
+
+                random = (int) Math.floor(Math.random() * (2));
+                answer3View.setText(reponses.get(buffer.get(random)).getReponse());
+                buffer.remove(random);
+
+                random = (int) Math.floor(Math.random() * (1));
+                answer4View.setText(reponses.get(buffer.get(random)).getReponse());
+                buffer.remove(random);
+            }
+        }
+
+        GetReponses getReponses = new GetReponses();
+        getReponses.execute();
+    }
+
+    private Question chooseRandomly(){
+        int random = (int) Math.floor(Math.random()*(availableQuestions.size()));
+
+        Question question = availableQuestions.get(random);
+        availableQuestions.remove(random);
+
+        return question;
     }
 }
